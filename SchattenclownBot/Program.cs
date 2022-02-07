0ab8d1b9543b60e5b0b65d06fbd1a3b5e4905486
Schattenclown <64093133+Schattenclown@users.noreply.github.com>
2022-02-07 14:05:29 +0100

    run infinite time? pls no manual restart needed?

--- SchattenclownBot/Program.cs
+++ SchattenclownBot/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using SchattenclownBot.Model.Discord;
 using SchattenclownBot.Model.Persistence;
 using SchattenclownBot.Model.Objects;
+using SchattenclownBot.Model.HelpClasses;
 
 namespace SchattenclownBot
 {
@@ -25,76 +26,21 @@ namespace SchattenclownBot
         {
             await Task.Run(async () =>
             {
-                DB_ScTimers.CreateTable_ScTimers();
-                DB_ScAlarmClocks.CreateTable_ScAlarmClocks();
+                try
+                {
+                    DB_ScTimers.CreateTable_ScTimers();
+                    DB_ScAlarmClocks.CreateTable_ScAlarmClocks();
 
-                dBot = new DiscordBot();
-#pragma warning disable CS4014 // Da auf diesen Aufruf nicht gewartet wird, wird die Ausführung der aktuellen Methode vor Abschluss des Aufrufs fortgesetzt.
-                dBot.RunAsync();
-#pragma warning restore CS4014 // Da auf diesen Aufruf nicht gewartet wird, wird die Ausführung der aktuellen Methode vor Abschluss des Aufrufs fortgesetzt.
-
-                await Task.Delay(1000 * 60 * 60 * 24);
-                Program p = new Program();
-                await p.RestartProgram();
-
+                    dBot = new DiscordBot();
+    #pragma warning disable CS4014 // Da auf diesen Aufruf nicht gewartet wird, wird die Ausführung der aktuellen Methode vor Abschluss des Aufrufs fortgesetzt.
+                    await dBot.RunAsync();
+    #pragma warning restore CS4014 // Da auf diesen Aufruf nicht gewartet wird, wird die Ausführung der aktuellen Methode vor Abschluss des Aufrufs fortgesetzt.
+                }
+                catch
+                {
+                    Reset.RestartProgram();
+                }
             });
-        }
-
-        /// <summary>
-        /// Centers the console.
-        /// </summary>
-        /// <param name="s">The text.</param>
-        static void Center(string s)
-        {
-            try
-            {
-                Console.Write("██");
-                Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
-                Console.Write(s);
-                Console.SetCursorPosition((Console.WindowWidth - 4), Console.CursorTop);
-                Console.WriteLine("██");
-            }
-            catch (Exception)
-            {
-                s = "Console to smoll EXC";
-                Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
-                Console.Write(s);
-                Console.SetCursorPosition((Console.WindowWidth - 4), Console.CursorTop);
-                Console.WriteLine("██");
-            }
-        }
-
-        /// <summary>
-        /// Restarts the program.
-        /// </summary>
-        private async Task RestartProgram()
-        {
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{"".PadRight(Console.WindowWidth - 2, '█')}");
-            Center(" ");
-            Center(@"██████╗ ███████╗███████╗████████╗ █████╗ ██████╗ ████████╗██╗███╗   ██╗ ██████╗ ");
-            Center(@"██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██║████╗  ██║██╔════╝ ");
-            Center(@"██████╔╝█████╗  ███████╗   ██║   ███████║██████╔╝   ██║   ██║██╔██╗ ██║██║  ███╗");
-            Center(@"██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██║██║╚██╗██║██║   ██║");
-            Center(@"██║  ██║███████╗███████║   ██║   ██║  ██║██║  ██║   ██║   ██║██║ ╚████║╚██████╔╝");
-            Center(@"╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
-            Center(" ");
-            Console.WriteLine($"{"".PadRight(Console.WindowWidth - 2, '█')}");
-            Center("Restating in 10 secounds.");
-            Console.WriteLine($"{"".PadRight(Console.WindowWidth - 2, '█')}");
-            await Task.Delay(1000 * 10);
-
-            dBot.Dispose();
-            // Get file path of current process 
-            var filePath = Assembly.GetExecutingAssembly().Location;
-            //var filePath = Application.ExecutablePath;  // for WinForms
-
-            // Start program
-            Process.Start(filePath);
-
-            // For all Windows application but typically for Console app.
-            Environment.Exit(0);
         }
     }
 }
