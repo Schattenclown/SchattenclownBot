38c0da8c6e094676cd71ecca23f2e37af0ba9b09
Schattenclown <64093133+Schattenclown@users.noreply.github.com>
2022-07-10 14:34:45 +0200

    lambda expressions not even hidden op, obvius OP

---
+++ SchattenclownBot/Model/AsyncFunction/GetItRightMee6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DisCatSharp;
+using DisCatSharp.Entities;
+using DisCatSharp.EventArgs;
+using SchattenclownBot.Model.Discord.Main;
+using SchattenclownBot.Model.HelpClasses;
+using DisCatSharp.Net.Models;
+namespace SchattenclownBot.Model.AsyncFunction
+{
+    internal class GetItRightMee6
+    {
+        internal static Task ItRight(DiscordClient client, ChannelCreateEventArgs e)
+        {
+            if (e.Channel.Name.Contains("🥇AFK-Farm#"))
+            {
+                e.Channel.ModifyAsync(x => x.Bitrate = 256000);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
