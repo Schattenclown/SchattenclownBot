13dccedd695b8adcd4de95c1f6a1fabd556d04fa
elbrodark <dsc.clan135@googlemail.com>
2022-09-16 19:30:27 +0200

    Update CSV_Connections.cs
    Path abgeändert, sobald nun eine `Connections.csv` im Hauptverzeichnis liegt kann darauf zugegriffen werden.
    
    Mounten kann man das ganze dann in "/app/Connections.csv"
    
    Die `docker-compose.yml` schaut dann so aus:
    
    ```yml title="docker-compose.yml"
    services:
    version: '3.4'
    
    services:
      oebibot:
        image: elbrodark/schattenclownbot:latest
        restart: unless-stopped
        volumes:
          - /mnt/user/appdata/oebibot_data/Connections2.csv:/app/Connections2.csv
    #                         Hostverzeichnis                                       :      Dockerverzeichnis
    ```

--- SchattenclownBot/Model/Persistence/Connection/CSV_Connections.cs
+++ SchattenclownBot/Model/Persistence/Connection/CSV_Connections.cs
@@ -1,4 +1,4 @@
-﻿using SchattenclownBot.Model.Objects;
+using SchattenclownBot.Model.Objects;
 using System;
 using System.IO;
 
@@ -6,113 +6,57 @@ namespace SchattenclownBot.Model.Persistence.Connection
 {
     public class CsvConnections
     {
-        private static readonly Uri Path = new($"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}/SchattenclownBot");
+        private static readonly Uri Path = new($"{Environment.CurrentDirectory}");
         private static readonly Uri Filepath = new($"{Path}/Connections.csv");
-
         public static Connections ReadAll()
         {
-            Connections connections = new();
-
-            if (Environment.GetEnvironmentVariable("ENV_SET").ToString() == "0")
+            try
             {
-                try
+                StreamReader streamReader = new(Filepath.LocalPath);
+                Connections connections = new();
+                while (!streamReader.EndOfStream)
                 {
-                    StreamReader streamReader = new(Filepath.LocalPath);
-                    while (!streamReader.EndOfStream)
+                    string row = streamReader.ReadLine();
+                    if (row != null)
                     {
-                        string row = streamReader.ReadLine();
-                        if (row != null)
-                        {
-                            string[] infos = row.Split(';');
+                        string[] infos = row.Split(';');
 
-                            switch (infos[0])
-                            {
-                                case "DiscordBotKey":
-                                    connections.DiscordBotKey = infos[1];
-                                    break;
-                                case "DiscordBotKeyDebug":
-                                    connections.DiscordBotDebug = infos[1];
-                                    break;
-                                case "MySqlConStr":
-                                    connections.MySqlConStr = infos[1].Replace(',', ';');
-                                    break;
-                                case "MySqlConStrDebug":
-                                    connections.MySqlConStrDebug = infos[1].Replace(',', ';');
-                                    break;
-                                case "AcoustIdApiKey":
-                                    connections.AcoustIdApiKey = infos[1];
-                                    break;
-                                case "SpotifyOAuth2":
-                                    connections.Token = new Connections.SpotifyOAuth2();
-                                    string[] spotifyOAuth2 = infos[1].Split('-');
-                                    connections.Token.ClientId = spotifyOAuth2[0];
-                                    connections.Token.ClientSecret = spotifyOAuth2[1];
-                                    break;
-                                case "YouTubeApiKey":
-                                    connections.YouTubeApiKey = infos[1];
-                                    break;
-                            }
+                        switch (infos[0])
+                        {
+                            case "DiscordBotKey":
+                                connections.DiscordBotKey = infos[1];
+                                break;
+                            case "DiscordBotKeyDebug":
+                                connections.DiscordBotDebug = infos[1];
+                                break;
+                            case "MySqlConStr":
+                                connections.MySqlConStr = infos[1].Replace(',', ';');
+                                break;
+                            case "MySqlConStrDebug":
+                                connections.MySqlConStrDebug = infos[1].Replace(',', ';');
+                                break;
+                            case "AcoustIdApiKey":
+                                connections.AcoustIdApiKey = infos[1];
+                                break;
+                            case "SpotifyOAuth2":
+                                connections.Token = new Connections.SpotifyOAuth2();
+                                string[] spotifyOAuth2 = infos[1].Split('-');
+                                connections.Token.ClientId = spotifyOAuth2[0];
+                                connections.Token.ClientSecret = spotifyOAuth2[1];
+                                break;
+                            case "YouTubeApiKey":
+                                connections.YouTubeApiKey = infos[1];
+                                break;
                         }
                     }
-                    streamReader.Close();
-                    return connections;
                 }
-                catch (Exception)
-                {
-                    DirectoryInfo directory = new(Path.LocalPath);
-                    if (!directory.Exists)
-                        directory.Create();
-
-                    StreamWriter streamWriter = new(Filepath.LocalPath);
-                    streamWriter.WriteLine("DiscordBotKey;<API Key here>\n" +
-                                           "DiscordBotKeyDebug;<API Key here>\n" +
-                                           "MySqlConStr;<DBConnectionString here>\n" +
-                                           "MySqlConStrDebug;<DBConnectionString here>\n" +
-                                           "AcoustIdApiKey;<Api Key here>\n" +
-                                           "SpotifyOAuth2;<ClientId-ClientSecret here>");
-
-                    streamWriter.Close();
-                    throw new Exception($"{Path.LocalPath}\n" +
-                                        "API key´s and database strings not configured!");
-                }
-            }
-            else if (Environment.GetEnvironmentVariable("ENV_SET").ToString() == "1")
-            {
-                /*
-                services:
-                version: '3.4'
-
-                services:
-                  oebibot:
-                    image: elbrodark/schattenclownbot:latest
-                    restart: unless-stopped
-                    environment:
-                    - ENV_SET="1"
-                    - DiscordBotKey=""
-                    - DiscordBotKeyDebug=""
-                    - MySqlConStr=""
-                    - MySqlConStrDebug=""
-                    - AcoustIdApiKey=""
-                    - SpotifyOAuth2=""
-                 *
-                 */
-
-                connections.DiscordBotKey = Environment.GetEnvironmentVariable("DiscordBotKey").ToString();
-                connections.DiscordBotDebug = Environment.GetEnvironmentVariable("DiscordBotDebug").ToString();
-                connections.MySqlConStr = Environment.GetEnvironmentVariable("MySqlConStr").ToString().Replace(',', ';');
-                connections.MySqlConStrDebug = Environment.GetEnvironmentVariable("MySqlConStrDebug").ToString().Replace(',', ';');
-                connections.AcoustIdApiKey = Environment.GetEnvironmentVariable("AcoustIdApiKey").ToString();
-                connections.Token = new Connections.SpotifyOAuth2();
-                string[] spotifyOAuth2 = Environment.GetEnvironmentVariable("SpotifyOAuth2").ToString().Split('-');
-                connections.Token.ClientId = spotifyOAuth2[0];
-                connections.Token.ClientSecret = spotifyOAuth2[1];
-                //connections.YouTubeApiKey = Environment.GetEnvironmentVariable("YouTubeApiKey").ToString();
-
-
-
+                streamReader.Close();
                 return connections;
             }
-            return null;
+            catch (Exception)
+            {
+                throw new Exception($"{Path.LocalPath}\n" + "API key´s and database strings not configured!");
+            }
         }
     }
 }
