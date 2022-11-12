30e6400f28e9ffc92c8f9833f1ea486fd5824513
Schattenclown <64093133+Schattenclown@users.noreply.github.com>
2022-11-12 14:46:53 +0100

    a

--- SchattenclownBot/Model/Objects/API.cs
+++ SchattenclownBot/Model/Objects/API.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 
 namespace SchattenclownBot.Model.Objects
 {
-    internal class API
-    {
-        public int PUTiD { get; set; }
-        public string Command { get; set; }
-        public DateTime RequestTimeStamp { get; set; }
-        public ulong RequestSecret { get; set; }
-
-        internal static List<API> GET()
-        {
-            return DB_API.GET();
-        }
-        internal static void DELETE(int pUTiD)
-        {
-            DB_API.DELETE(pUTiD);
-        }
-    }
+   internal class API
+   {
+      public int CommandRequestID { get; set; }
+      public ulong RequestDiscordUserId { get; set; }
+      public ulong RequestSecretKey { get; set; }
+      public DateTime RequestTimeStamp { get; set; }
+      public string Command { get; set; }
+      internal static List<API> GET()
+      {
+         return DB_API.GET();
+      }
+      internal static void DELETE(int commandRequestID)
+      {
+         DB_API.DELETE(commandRequestID);
+      }
+   }
 }
