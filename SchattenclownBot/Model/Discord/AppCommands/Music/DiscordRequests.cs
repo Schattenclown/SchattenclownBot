2ebcf3db58a42d02c0e0a9f35bc9acf901676e54
Schattenclown <64093133+Schattenclown@users.noreply.github.com>
2022-11-20 15:40:18 +0100

    Update DiscordRequests.cs

--- SchattenclownBot/Model/Discord/AppCommands/Music/DiscordRequests.cs
+++ SchattenclownBot/Model/Discord/AppCommands/Music/DiscordRequests.cs
@@ -9,7 +9,6 @@ using SchattenclownBot.Model.Discord.Main;
 
 namespace SchattenclownBot.Model.Discord.AppCommands.Music;
 
-[SlashCommandGroup("music", "Music commands", dmPermission: false)]
 internal class DiscordRequests : ApplicationCommandsModule
 {
    [SlashCommand("Play" + Bot.isDevBot, "Play Spotify or YouTube links!")]
