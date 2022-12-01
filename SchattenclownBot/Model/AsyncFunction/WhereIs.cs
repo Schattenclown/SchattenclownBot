8d8da3dae71788c8b42de6efdeb268164ccc0c78
Schattenclown <64093133+Schattenclown@users.noreply.github.com>
2022-12-01 21:31:18 +0100

    Update WhereIs.cs

--- SchattenclownBot/Model/AsyncFunction/WhereIs.cs
+++ SchattenclownBot/Model/AsyncFunction/WhereIs.cs
@@ -32,7 +32,7 @@ internal class WhereIs
          } while (guildList.Count == 0);
 
          DiscordGuild mainGuild = Bot.DiscordClient.GetGuildAsync(928930967140331590).Result;
-         DiscordChannel discordChannelOtherPlaces = mainGuild.GetChannel(987123289619071026);
+         DiscordChannel discordChannelOtherPlaces = mainGuild.GetChannel(928937150546853919);
          guildList.Remove(Bot.DiscordClient.GetGuildAsync(858089281214087179).Result);
          guildList.Remove(Bot.DiscordClient.GetGuildAsync(918232272732319744).Result);
 
